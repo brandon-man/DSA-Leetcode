ff6f6fb6618f0712216ac7bdd21485b5e7d89179
Brandon Man <nybrandonman@gmail.com>
2022-06-15 15:31:21 -0700

    Time: 113 ms (97.72%), Space: 53.8 MB (45.72%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let hash = {};
+    
+    for (let str of strs) {
+        let key = [...str].sort().join("");
+        if (!hash[key]) {
+            hash[key] = [];
+        }
+        hash[key].push(str);
+    }
+    return Object.values(hash);
+};
+
+/**
+* create a key with the value being the sorted str 
+* if there is no value, create an empty array
+* push the str into array
+* return hash as an array
+*/
