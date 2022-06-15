62dbde01f97c2890ebebd36011e58c8672b67238
Brandon <86130943+brandon-man@users.noreply.github.com>
2022-06-14 18:31:22 -0700

    Time: 149 ms (47.06%), Space: 55.6 MB (34.19%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let res = [];
+    let prefix = 1, postfix = 1;
+    
+    for (let i = 0; i < nums.length; i++) {
+        res[i] = prefix;
+        prefix *= nums[i];
+    }
+    
+    for (let j = nums.length - 1; j >= 0; j--) {
+        res[j] *= postfix;
+        postfix *= nums[j];
+    }
+    return res;
+};
