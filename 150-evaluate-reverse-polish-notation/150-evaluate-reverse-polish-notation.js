42a4ac721610e75413403814ecaa4ee8b734d225
Brandon Man <nybrandonman@gmail.com>
2022-06-23 20:26:57 -0700

    Time: 101 ms (53.63%), Space: 46 MB (23.81%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -3,25 +3,24 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-      const stack = [];
-  
-  for (let token of tokens) {
-    switch(token) {
-      case '+':
-        stack.push(stack.pop()+stack.pop());
-        break;
-      case '-':
-         stack.push(-stack.pop()+stack.pop());
-        break;
-      case '*':
-        stack.push(stack.pop()*stack.pop());
-        break;
-      case '/':
-        stack.push(Math.trunc(1/stack.pop()*stack.pop()));
-        break;
-      default:
-        stack.push(parseInt(token));
+    let stack = [];
+    for (let token of tokens) {
+        switch (token) {
+            case "+":
+                stack.push(stack.pop() + stack.pop());
+                break;
+            case "-":
+                stack.push(-stack.pop() + stack.pop());
+                break;
+            case "*":
+                stack.push(stack.pop() * stack.pop());
+                break;
+            case "/":
+                stack.push(Math.trunc(1 / stack.pop() * stack.pop()));
+                break;
+            default:
+                stack.push(parseInt(token))
+        }
     }
-  }
     return stack[0];
 };
