047ccf0a13070f3292c3a9e03ed35f0f4e24a7b4
Brandon Man <nybrandonman@gmail.com>
2022-06-29 21:52:09 -0700

    Time: 465 ms (36.61%), Space: 72.5 MB (17.19%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.js
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -3,14 +3,20 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    let res = Array.from({length:temperatures.length},x=>0);
-    let stack = [];
-    for(let i=0; i<temperatures.length; i++){
-    	while(stack.length>0 && temperatures[stack[stack.length-1]]<temperatures[i]){
-    		let j = stack.pop();
-    		res[j] = i-j;
-    	}
-    	stack.push(i);
+    const res = new Array(temperatures.length).fill(0);
+    const stack = [];
+    
+    for (let i = 0; i < temperatures.length; i++) {
+        const currTemp = temperatures[i];
+        
+        while(stack.length && stack[stack.length - 1][0] < currTemp) {
+            const [poppedTemp, poppedIndex] = stack.pop();
+            
+            res[poppedIndex] = i - poppedIndex;
+        }
+        
+        stack.push([currTemp, i])
     }
+    
     return res;
 };
