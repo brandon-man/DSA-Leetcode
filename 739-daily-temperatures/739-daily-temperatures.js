0b9fb125ef812fb059eb97df9ce677b290988859
Brandon Man <nybrandonman@gmail.com>
2022-07-03 19:02:18 -0700

    Time: 441 ms (40.21%), Space: 75.1 MB (9.41%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.js
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -3,23 +3,15 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    // create an array filled with 0s
-    let result = new Array(temperatures.length).fill(0);
+    let res = new Array(temperatures.length).fill(0);
     let stack = [];
     
-    // iterate thru each temp
     temperatures.forEach((temp, index) => {
-        // while stack is not empty and the top of the stack is less than temp
-        while (stack.length && temperatures[stack[stack.length - 1]] < temp) {
-            // pop prev index from stack
-            let prev = stack.pop();
-            // prev index assigned to result is now index minus prev index
-            // calculates how many days until temp increases
-            result[prev] = index - prev;
+        while (stack.length && temp > stack[stack.length - 1][0]) {
+            const [stackT, stackIdx] = stack.pop();
+            res[stackIdx] = index - stackIdx;
         }
-        // push index into stack
-        stack.push(index);
-    });
-    
-    return result;
+        stack.push([temp, index]);
+    })
+    return res;
 };
