1e5de3d62e01f2ea0e5d3ad9e296cde8ce4b9ed5
Brandon Man <nybrandonman@gmail.com>
2022-07-09 22:34:01 -0700

    Time: 76 ms (89.91%), Space: 43.4 MB (22.15%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,14 +5,11 @@
  */
 var twoSum = function(nums, target) {
     let map = new Map();
-    
     for (let i = 0; i < nums.length; i++) {
-        let current = nums[i];
-        let complement = target - current;
-        
+        let complement = target - nums[i];
         if (map.has(complement)) {
-            return [i, map.get(complement)]
+            return [i, map.get(complement)];
         }
-        map.set(current, i)
+        map.set(nums[i], i);
     }
 };
