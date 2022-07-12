f6d3d08ebfe1acb4c48869fd25423e2a3c2ab3f3
Brandon Man <nybrandonman@gmail.com>
2022-07-11 21:28:08 -0700

    Time: 314 ms (75.29%), Space: 73.4 MB (57.83%) - LeetHub

--- 853-car-fleet/853-car-fleet.js
+++ 853-car-fleet/853-car-fleet.js
@@ -5,21 +5,23 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
-     let hash = new Map()//Use a hash map so we dont need to do stupid array shit
-    for(let i = 0; i<position.length; i++){
-        hash.set(position[i], speed[i])
-    }
-    position.sort(function (a,b) {return a-b})//We need to sort array so we are looking at closest to farthest
+    let hash = new Map();
     
-    let stack = []
-    
-    stack.push(position.pop())
-    
-    while(position.length){
-        let fleetAhead = stack[stack.length-1]
-        let fleetBehind = position.pop()
-        if((target-fleetAhead)/hash.get(fleetAhead) < ((target)-fleetBehind)/hash.get(fleetBehind)) stack.push(fleetBehind) //Distance/vel = time, compare time. If time ahead is greater than time behind, then car will be behind car ahead. Otherwise, push fleet
+    for (let i = 0; i < position.length; i++) {
+        hash.set(position[i], speed[i]);
     }
     
-    return stack.length
+    position.sort((a, b) => a - b);
+    
+    let stack = [];
+    
+    stack.push(position.pop());
+    
+    while (position.length) {
+        let fleetAhead = stack[stack.length - 1];
+        let fleetBehind = position.pop();
+        if ((target - fleetAhead) / hash.get(fleetAhead) < ((target) - fleetBehind) / hash.get(fleetBehind))
+            stack.push(fleetBehind)
+    }
+    return stack.length;
 };
