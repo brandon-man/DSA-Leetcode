9aba7a87812115cb3dbc17241cc145c88527c8c5
Brandon Man <nybrandonman@gmail.com>
2022-07-18 20:59:56 -0700

    Time: 88 ms (61.26%), Space: 45.5 MB (15.12%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,9 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            return i;
+    let left = 0, right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
         }
     }
     return -1;
