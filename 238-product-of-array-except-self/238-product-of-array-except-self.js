28c51f7dbdeebed8f35b82a229dcacda91e286e8
Brandon Man <nybrandonman@gmail.com>
2022-07-28 23:43:27 -0700

    Time: 113 ms (89.35%), Space: 55.7 MB (27.90%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -4,16 +4,19 @@
  */
 var productExceptSelf = function(nums) {
     let res = [];
+    // create prefix and postfix at 0
     let prefix = 1, postfix = 1;
-    
+    // iterate thru nums
     for (let i = 0; i < nums.length; i++) {
+        // assign prefix to new array
         res[i] = prefix;
-        prefix *= nums[i];
+        prefix *= nums[i]
     }
-    
+    // iterate thru nums
     for (let j = nums.length - 1; j >= 0; j--) {
+        // multiply postfix
         res[j] *= postfix;
-        postfix *= nums[j];
+        postfix *= nums[j]
     }
     return res;
 };
