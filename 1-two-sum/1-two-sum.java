5622c06cc794db5c210e8369ab94631f7bf5daf0
Brandon Man <nybrandonman@gmail.com>
2022-08-16 16:09:05 -0700

    Time: 3 ms (92.36%), Space: 46.2 MB (19.47%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -3,11 +3,11 @@ class Solution {
         HashMap<Integer, Integer> hash = new HashMap<>();
         
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (hash.containsKey(nums[i])) {
-                return new int[] { hash.get(nums[i]), i };
+            int diff = target - nums[i];
+            if (hash.containsKey(diff)) {
+                return new int[] { i, hash.get(diff) };
             }
-            hash.put(complement, i);
+            hash.put(nums[i], i);
         }
         return new int[] {};
     }
