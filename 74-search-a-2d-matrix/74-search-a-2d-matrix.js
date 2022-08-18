5989d79ba1184e316097c15d044ffc29a95f4793
Brandon Man <nybrandonman@gmail.com>
2022-08-18 15:25:46 -0700

    Time: 84 ms (61.01%), Space: 42.5 MB (19.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -15,14 +15,11 @@ var searchMatrix = function(matrix, target) {
         const [row, col] = [(Math.floor(mid / cols)), (mid % cols)];
         const guess = matrix[row][col];
         
-        const isTarget = guess === target;
-        if (isTarget) return true;
+        if (guess === target) return true;
         
-        const isTargetGreater = guess < target;
-        if (isTargetGreater) left = mid + 1;
+        if (guess < target) left = mid + 1;
 
-        const isTargetLess = target < guess;
-        if (isTargetLess) right = mid - 1;
+        if (target < guess) right = mid - 1;
     }
     return false;
 };
