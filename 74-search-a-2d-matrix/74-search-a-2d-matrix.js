a553626117cf4c86bce76c8e1ddd671fc842d693
Brandon Man <nybrandonman@gmail.com>
2022-08-22 17:34:05 -0700

    Time: 90 ms (50.28%), Space: 42.2 MB (46.30%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,16 +4,15 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    // rows = length of matrix, cols = first index of each arrays inside matrix
+    // determine rows and cols
     const [rows, cols] = [matrix.length, matrix[0].length];
-    // left = first pointer in array, right = rows * cols - 1
+    // start from beginning and end of 2D matrix
     let [left, right] = [0, ((rows * cols) - 1)];
     
     while (left <= right) {
-        // ???
-        const mid = Math.floor((left + right) / 2);
-        const [row, col] = [(Math.floor(mid / cols)), (mid % cols)];
-        const guess = matrix[row][col];
+         const mid = Math.floor((left + right) / 2);
+         const [row, col] = [(Math.floor(mid / cols)), (mid % cols)]
+         const guess = matrix[row][col];
         
         if (guess === target) return true;
         
