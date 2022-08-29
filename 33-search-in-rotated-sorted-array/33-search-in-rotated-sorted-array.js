ae45fce1c2545b6306fc9b98db9bd6fdd9a2687e
Brandon Man <nybrandonman@gmail.com>
2022-08-29 12:09:13 -0700

    Time: 115 ms (12.14%), Space: 42 MB (68.49%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,23 +4,21 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0, right = nums.length - 1;
+    let [left, right] = [0, nums.length - 1];
     
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
         
-        if (target === nums[mid]) {
-            return mid;
-        }
+        if (nums[mid] === target) return mid;
         
         if (nums[left] <= nums[mid]) {
-            if (target > nums[mid] || target < nums[left]) {
+            if (target < nums[left] || target > nums[mid]) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         } else {
-            if (target < nums[mid] || target > nums[right]) {
+            if (target > nums[right] || target < nums[mid]) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
