668e381de8b476ec5470e52eec15a59f2fa956d5
Brandon Man <nybrandonman@gmail.com>
2022-09-01 11:21:52 -0700

    Time: 108 ms (17.20%), Space: 41.6 MB (93.75%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -3,5 +3,27 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    return Math.min(...nums)
+    let [left, right] = [0, nums.length - 1];
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        
+        let target = Math.min(...nums);
+        
+        if (nums[mid] === target) return nums[mid];
+        
+        if (nums[left] <= nums[mid]) {
+            if (target < nums[left] || target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            if (target > nums[right] || target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
 };
