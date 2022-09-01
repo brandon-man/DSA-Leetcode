f118d0ffb837aed57fdaf25b60c148d9491f9b9d
Brandon Man <nybrandonman@gmail.com>
2022-09-01 10:43:37 -0700

    Time: 113 ms (13.68%), Space: 42.3 MB (42.65%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -12,13 +12,13 @@ var search = function(nums, target) {
         if (nums[mid] === target) return mid;
         
         if (nums[left] <= nums[mid]) {
-            if (target > nums[mid] || target < nums[left]) {
+            if (target < nums[left] || target > nums[mid]) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         } else {
-            if (target < nums[mid] || target > nums[right]) {
+            if (target > nums[right] || target < nums[mid]) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
